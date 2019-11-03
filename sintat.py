b70d3ac511873f5ee0c58d33a2a882e55cc7b977
JP <joao@geekbusinesslab.com.br>
2019-11-03 03:46:25 -0300

    sintatico finalizado

--- sintat.py
+++ sintat.py
@@ -1,29 +1,104 @@
 import json
 
-f = open('rl_table.json', 'r')
-entrada = f.read()
-entradaJson = json.loads(entrada)
+f = open('rl_tableFINAL.json', 'r')
+tabelaLR = json.loads(f.read())
 
-tableLR = []
-array = {}
+f = open('gramatica.json', 'r')
+gramatica = json.loads(f.read())
 
-for reg in entradaJson:
-    for key in reg:
-        b =(reg[key]).replace(u'\xa0', u' ')
-        a =(key).replace(u'\xa0', u' ')
-        #print(a, b)
-        array[a] = b
-    tableLR.append(array)
-    array = {}
+entradaLexico = [
+    {'programainicio': 'programainicio'},
+    {'definainstrucao': 'definainstrucao'},
+    {'identificador': 'Trilha'},
+    {'como': 'como'},
+    {'inicio': 'inicio'},
+    {'mova': 'mova'},
+    {'numero': '2'},
+    {'passos': 'passos'},
+    {'aguarde': 'aguarde'},
+    {'ate': 'ate'},
+    {'robo': 'robo'},
+    {'pronto': 'pronto'},
+    {'vire': 'vire'},
+    {'para': 'para'},
+    {'esquerda': 'esquerda'},
+    {'apague': 'apague'},
+    {'lampada': 'lampada'},
+    {'vire': 'vire'},
+    {'para': 'para'},
+    {'direita': 'direita'},
+    {'mova': 'mova'},
+    {'numero': '3'},
+    {'passos': 'passos'},
+    {'aguarde': 'aguarde'},
+    {'ate': 'ate'},
+    {'robo': 'robo'},
+    {'pronto': 'pronto'},
+    {'fim': 'fim'},
+    {'execucaoinicio': 'execucaoinicio'},
+    {'vire': 'vire'},
+    {'para': 'para'},
+    {'direita': 'direita'},
+    {'fimexecucao': 'fimexecucao'},
+    {'fimprograma': 'fimprograma'}
+]
 
-print(tableLR[1])
+def trataShift(action, token):
+    novoEstado = int(action[1:])
+    pilha.append(token)
+    pilha.append(novoEstado)
 
-entradaLexico = 'programainicio'
+def trataReduce(action, token):
+
+    # pilha.pop()
+    # pilha.pop()
+    linhaInstrucao = int(action[1:]) 
+
+    instrucao = gramatica[linhaInstrucao]["token"]
+    value = gramatica[linhaInstrucao]["value"]
+
+    if value != "":
+        num_consume = len(value.split(" "))
+
+        for i in range(num_consume):
+            pilha.pop()
+            pilha.pop()
+    
+    ultimoEstado = pilha[getTopoIdx()]
+    # while len(pilha) > 0 and estado == "":
+    #     pilha.pop()
+    #     pilha.pop()
+    #     ultimoEstado = pilha[getTopoIdx()]
+    #     estado = tabelaLR[ultimoEstado][instrucao]
+
+    estado = int(tabelaLR[ultimoEstado][instrucao])
+    pilha.append(instrucao)
+    pilha.append(estado)
+
+    newAction = tabelaLR[estado][token]
+    print((newAction, token))
+
+    if 's' in newAction:
+        trataShift(newAction, token)
+    else:
+        trataReduce(newAction, token)
+
+
+def getTopoIdx():
+    return len(pilha) - 1
 
 pilha = []
-
-pilha.append('$')
 pilha.append(0)
+
+for token in entradaLexico:
+    estado = pilha[getTopoIdx()]
+    token = list(token)[0]
+    action = tabelaLR[estado][token]
+
+    if 's' in action:
+        trataShift(action, token)
+    else:
+        trataReduce(action, token)
 
 
 
