b7c91348505df5b2f92b8c70c4548cd8b5a55535
holynacll <alexandrecurylima@hotmail.com>
2019-11-06 21:18:20 -0300

    semantico feito

--- sintatico/no.py
+++ sintatico/no.py
@@ -1,6 +1,7 @@
 class No:
     filhos: []
     chave: str
-    def __init__(self, chave, filhos = []):
+    def __init__(self, chave, nome, filhos = []):
         self.chave = chave
+        self.nome = nome
         self.filhos = filhos
--- sintatico/sintat.py
+++ sintatico/sintat.py
@@ -1,5 +1,8 @@
 import json
 import sys
+import re
+
+sys.path.insert(1, '../lexico/')
 import lexico_funcao
 
 import tabela_de_simbolos as ts
@@ -8,30 +11,46 @@ import no
 symbol_table = []
 nome_table = []
 arvore = []
+arvore_semantica = []
 nome_table.append(ts.TabelaDeSimbolos())
 symbol_table.append(nome_table[len(symbol_table)])
+tokens_name = ['PROG', 'DECL', 'INSTR', 'BLOCO', 'COND', 'ITER', 'LACO', 'COMANDO', 'COMANDOS', 'IFELSE', 'IF', 'ELSE','SENTIDO']
 
 class Node:
-    def __init__(self, value=None, children=None):
+    def __init__(self, value=None, nome=None, children=None):
         if children is None:
             children = []
-        self.value, self.children = value, children
+        self.value, self.nome, self.children = value, nome, children
 
 #table.add(x,y)
 #table.search(x)
 
 entradaLexico = lexico_funcao.lexico()
 
-f = open('sintatico/rl_tableFINAL.json', 'r')
+f = open('rl_tableFINAL.json', 'r')
 tabelaLR = json.loads(f.read())
 
-f = open('sintatico/gramatica.json', 'r')
+f = open('gramatica.json', 'r')
 gramatica = json.loads(f.read())
 
-def printaArvore(arvore):
-    root = Node("PROG")
-    root.children = transformaFilhos(arvore)
+def percorreArvoreSemantica(node, file=None, _prefix="", _last=True):
+    if node.nome == 'PRODUCAO':
+        print(_prefix, "`- " if _last else "|- ", '|', sep="", file=file)
+    else:
+        print(_prefix, "`- " if _last else "|- ", node.nome, sep="", file=file)
+    _prefix += "   " if _last else "|  "
+    child_count = len(node.children)
+    for i, child in enumerate(node.children):
+        _last = i == (child_count - 1)
+        percorreArvoreSemantica(child, file, _prefix, _last)
 
+
+def printaArvore(arvore):
+    root = Node("PROG", 'PRODUCAO')
+    print(root.__dict__)
+    root.children = transformaFilhos(arvore)
+    print(root.__dict__)
+    percorreArvoreSemantica(root)
     pprint_tree(root)
 
 def pprint_tree(node, file=None, _prefix="", _last=True):
@@ -47,7 +66,7 @@ def transformaFilhos(filhos):
     nodeFilhos = []
 
     for f in filhos:
-        no = Node(f.chave)
+        no = Node(f.chave, f.nome)
         if not hasattr(f, 'filhos') or len(f.filhos) == 0:
             no.children = []    
         else: 
@@ -111,9 +130,11 @@ def montaArvore(arvore):
 
 def trataShift(action, token, tokenIdx, tokenData):
     # print(action, token, tokenIdx, tokenData, int(action[1:]))
-
+    
+    arvore_semantica.append(tokenData[token])
+    
     global arvore
-    arvore.append(no.No(token))
+    arvore.append(no.No(token, tokenData[token]))
 
     novoEstado = int(action[1:])
     pilha.append(token)
@@ -122,6 +143,8 @@ def trataShift(action, token, tokenIdx, tokenData):
     count = 0
     idx_token = 0
     existe_token = True
+#    if 'identificador' in tokenData.keys() or 'numero' in tokenData.keys():
+#        print(token, tokenData)
     for t in symbol_table:
         if t.search(token):
             count = count + 1
@@ -140,10 +163,10 @@ def trataShift(action, token, tokenIdx, tokenData):
         symbol_table[idx_token].add(token,tokenData)
 
 
-    # for t in symbol_table:
-        # print('Tabela: ', count)
-        # count = count + 1
-        # print(t.__dict__)
+    #for t in symbol_table:
+    #    print('Tabela: ', count)
+    #    count = count + 1
+    #    print(t.__dict__)
 
 def trataReduce(action, token, tokenIdx, tokenData):
     linhaInstrucao = int(action[1:]) 
@@ -153,7 +176,7 @@ def trataReduce(action, token, tokenIdx, tokenData):
 
     global arvore
 
-    novoNo = no.No(instrucao)
+    novoNo = no.No(instrucao, 'PRODUCAO')
     if value != "":
 
         num_consume = len(value.split(" "))
@@ -235,8 +258,35 @@ for tokenIdx in range(len(entradaLexico)):
 
 # outputArvore = montaArvore(arvore)
 # print(outputArvore)
+#for x in arvore:
+#    print(x.__dict__)
 
 
+#Analise Semantica
+semantica_string = ''
+for i, word in enumerate(arvore_semantica):
+    semantica_string = semantica_string + word
+    if i != len(arvore_semantica):
+        semantica_string = semantica_string + ' '
+
+
+#regra semantica do vire para em sentidos opostos
+x = re.search("vire para esquerda vire para direita", semantica_string)
+x2 = re.search("vire para direita vire para esquerda", semantica_string)
+
+#regra semantica para 2 ou mais declaracoes com mesmo nome
+y = re.findall("definainstrucao [a-zA-Z]+", semantica_string)
+
+mySet = set(y)
+if len(mySet) == len(y):
+    print("erro semantico: ha duas declaracoes de instrucoes com mesmo nome")
+
+if x or x2:
+    print('erro semantico: ha ocorrencia de vire para em sentidos opostos seguidas')
+#print(semantica_string)
+
+
+#prints das arvores
 printaArvore(arvore)
 print("OK")
 quit()
